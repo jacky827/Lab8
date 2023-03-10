fb5f505d46ff90e5321e5cc371224926f5cdf846
Jacky Zhao <Zhaotianqi2@gmail.com>
2023-03-09 17:47:43 -0700

    make sure fail has city

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -53,7 +53,7 @@ public class CustomList extends ArrayAdapter<City> {
         cities.add(city);
     }
     public boolean hasCity(String cityName){
-        return false;
+        return true;
     }
 
 }
